3fce9b355bf784ffe483a16c26644b6d50057484
taalojarvi <sreedevan05@gmail.com>
2022-05-20 21:32:44 +0530

    Week 3: Programming Assignment 2
    You are given a list L. Print the list of first 3 smallest elements in ascending order and last 2 greatest elements in descending order of the list L respectively.(We will take care of the input)
    
    Input
    
    A list L
    
    Output
    
    A list of first 3 smallest elements in L in ascending order
    A list of last 2 greatest elements in L in descending order
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

---
+++ W3-A2-Elements.py
@@ -0,0 +1,11 @@
+L = [int(i) for i in input().split()]
+X=sorted(L)
+Y=sorted(L,reverse=True)
+Small=[]
+Great=[]
+for i in range(3):
+	Small.append(X[i])
+for i in range(2):
+	Great.append(Y[i])
+print(Small)
+print(Great)
