d02df787f5d986c98dacb32b7c1a92389b2b7854
taalojarvi <sreedevan05@gmail.com>
2022-05-20 21:41:03 +0530

    Week 5: Programming Assignment 2
    You are given a list L. Write a function uniqueE which will return a list of unique elements is the list L in sorted order. (Unique element means it should appear in list L only once.)
    
    Input will be handled by us
    
    Input
    [1,2,3,3,4,4,2,5,6,7]
    
    Output
    [1,5,6,7]
    
    Explanation
    
    Elements 1,5,6,7 appears in the input list only once.
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

---
+++ W5-A2.py
@@ -0,0 +1,16 @@
+def uniqueE(L):
+  freq={}
+  uni=[]
+  
+  for item in L:
+    freq[item] = freq.get(item, 0) + 1
+#  print(freq)
+    
+  for temp in freq:
+#    print(freq[temp])
+    if(freq[temp]) == 1:
+      uni.append(temp)
+  uni.sort()
+  return uni
+L = [int(i) for i in input().split()]
+print(uniqueE(L))
