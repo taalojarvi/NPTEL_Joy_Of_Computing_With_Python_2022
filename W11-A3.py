ba633ed88b750c7d7244c440b9564c5f086c701c
taalojarvi <sreedevan05@gmail.com>
2022-05-20 22:00:00 +0530

    Week 11: Programming Assignment 3
    Write a program which takes two integer a and b and prints all composite numbers between a and b.(both numbers are inclusive)
    
    Input:
    10
    20
    
    Output:
    10
    12
    14
    15
    16
    18
    20
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

---
+++ W11-A3.py
@@ -0,0 +1,17 @@
+a=int(input())
+b=int(input())
+
+comp=[]
+prime=[]
+
+count=int(0)
+
+for x in range(a,b+1):
+  count=int(0)
+  
+  for y in range (2,x+1):
+    if(x % y == 0):
+      count += 1
+      
+  if(count >= 2):
+    print(x)
