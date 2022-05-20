6afcf4310356c6afa86518d889edd9299c62c324
taalojarvi <sreedevan05@gmail.com>
2022-05-20 22:03:13 +0530

    Week 12: Programming Assignment 3
    Given a student's roll number in the following format rollNumber@institute.edu.in, write a program to find the roll number and institute name of the student.
    
    Input:
    roll@institute.edu.in
    
    Output:
    roll institute
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>

---
+++ W12-A3.py
@@ -0,0 +1,21 @@
+L=str(input())
+
+instit=str() 
+length=len(L)
+counter=int(0)
+roll=str() 
+for i in range(length):
+  if (L[i] == "@"):
+    counter=i
+    break
+  else:
+    roll+=L[i]
+    
+
+for j in range(counter+1,length):
+  if(L[j] == "."):
+    break
+  else:
+    instit+=L[j] 
+    
+print(roll+" "+instit, end="" ) 
